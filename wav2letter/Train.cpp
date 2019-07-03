dde4ac7ee8544d09c3b73049e77c7e02fa0753f4
MarkDana <dhy326@gmail.com>
2019-07-03 20:08:18 +0800

    debugging

--- wav2letter/Train.cpp
+++ wav2letter/Train.cpp
@@ -222,7 +222,7 @@ int main(int argc, char** argv) {
       //auto m = af::constant(0.1,noiseDims);
       //auto m=fl::normal(noiseDims,0.002,0.1).array();
       // float mylr = 0.001;
-      float mylr = 0.01;
+      float mylr = 0.1;
 
       //the previous network's output f*
       fl::Variable preOutput; 
