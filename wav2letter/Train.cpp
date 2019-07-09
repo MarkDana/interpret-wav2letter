308063e46df3134c8103e97a7f98ba2ca02c01e9
MarkDana <dhy326@gmail.com>
2019-07-09 23:07:05 +0800

    debugging

--- wav2letter/Train.cpp
+++ wav2letter/Train.cpp
@@ -637,7 +637,7 @@ int main(int argc, char** argv) {
 
 
       af::sync();
-      if (FLAGS_reportiters == 0) {
+      // if (FLAGS_reportiters == 0) {
         //runValAndSaveModel(curEpoch, netopt->getLr(), critopt->getLr());
         //std::string mpath = "/root/w2l/aboutM/last_m.bin";
         //W2lSerializer::save(mpath, m);
@@ -648,7 +648,7 @@ int main(int argc, char** argv) {
           mfile << af::toString("last m is:", m);
           mfile.close();
         }
-      }
+      // }
     }
   };
 
