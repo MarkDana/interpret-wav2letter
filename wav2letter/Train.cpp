cdb573535543aec8e133087a2b37106e70959877
MarkDana <dhy326@gmail.com>
2019-07-19 20:56:44 +0800

    debugging

--- wav2letter/Train.cpp
+++ wav2letter/Train.cpp
@@ -478,9 +478,9 @@ int main(int argc, char** argv) {
         af::array f1_2 = absTiled*(sum_m_p_j - sum_mpj_partial_to_mpj*(MTiled-af::abs(iloop-ploop)))/(sum_m_p_j*sum_m_p_j); //i!=p, grad
 
 
-        af::array original_ratio_to_nowsum = absTiled/af::tile(af::sum(cond * f1_1,2),af::dim4(1, 1, K));
-        f1_1 *= original_ratio_to_nowsum;
-        f1_2 *= (original_ratio_to_nowsum*(1-f1_1*cond*original_ratio_to_nowsum/absTiled));
+        // af::array original_ratio_to_nowsum = absTiled/af::tile(af::sum(cond * f1_1,2),af::dim4(1, 1, K));
+        // f1_1 *= original_ratio_to_nowsum;
+        // f1_2 *= (original_ratio_to_nowsum*(1-f1_1*cond*original_ratio_to_nowsum/absTiled));
 
 
         af::array f2_1 = (-1.0)*af::tile(af::sum(cond * ((1 - i_e_p) * f1_1), 2), af::dim4(1, 1, K)); //i==p, add
@@ -631,7 +631,7 @@ int main(int argc, char** argv) {
 
   // printf("backward okok\n");
 
-       float lambda = 1e-10;
+       float lambda = 1e-11;
         //float lambda = 100;
         auto f_L2 = fl::norm(softmax_add_preOutput - softmax_add_output, {0,1});
         auto m_entropy = af::sum<float> (af::log(af::abs(m))); 
